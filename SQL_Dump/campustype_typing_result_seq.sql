db91678720fe7b1d074e2d6455dcabd9c1b5b416
Oulei <olivier.deszynski@ict-campus.net>
2025-04-18 12:24:44 +0200

    Ready for Railway deploy

--- SQL_Dump/campustype_typing_result_seq.sql
+++ SQL_Dump/campustype_typing_result_seq.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: localhost    Database: campustype
+-- Host: localhost    Database: railway
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
